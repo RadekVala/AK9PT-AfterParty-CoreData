a10dfa3a6a7f38f294b523943af10ebfa941581d
Radek Vala <radek.www@gmail.com>
2023-11-03 09:26:54 +0100

    Initial Commit

---
+++ AfterParty/AfterPartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfterPartyApp.swift
+//  AfterParty
+//
+//  Created by Radek Vala on 03.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct AfterPartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
